5ee063fe55b717150a8a2e71ab1ba1c74bed3742
Sabrina Nawaz <sabrinanawaz23@gmail.com>
2022-11-06 16:40:20 -0500

    Created tests for Employee and initialized the Employee class

---
+++ _tests_/Employee.test.js
@@ -0,0 +1,30 @@
+const Employee = require('../lib/Employee');
+const employee = Employee();
+
+test('employee has a name', () => {
+    expect(employee.name).toBe('Sabrina');
+});
+
+test('employee has an id', () => {
+    expect(employee.id).toBe(7);
+});
+
+test('employee has an email', () => {
+    expect(employee.email).toBe('sn@gmail.com');
+});
+
+test('employee has a getName function', () => {
+    expect(typeof employee.getName).toBe('function');
+});
+
+test('employee has a getId function', () => {
+    expect(typeof employee.getId).toBe('function');
+});
+
+test('employee has a getEmail function', () => {
+    expect(typeof employee.getEmail).toBe('function');
+});
+
+test('employee has a getRole function', () => {
+    expect(typeof employee.getRole).toBe('function');
+});

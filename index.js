2845dfec5210bd785830473eea1d519bc6b4fc89
Sabrina Nawaz <sabrinanawaz23@gmail.com>
2022-11-08 12:48:33 -0500

    Included inquirer prompt option to add another employee and created html page with styling and a function to generate page

--- index.js
+++ index.js
@@ -35,9 +35,7 @@ const addEngineerOrIntern = [
         name: "type",
         choices: ['engineer', 'intern']
     }
-
 ];
-
 //THEN I am prompted to enter the engineer’s name, ID, email, and GitHub username, and I am taken back to the menu
 const engineerQuestions = [
     {
@@ -61,7 +59,6 @@ const engineerQuestions = [
         name: "github",
     },
 ]
-
 //THEN I am prompted to enter the intern’s name, ID, email, and school, and I am taken back to the menu
 const internQuestions = [
     {
@@ -86,15 +83,22 @@ const internQuestions = [
     },
 ]
 inquirer.prompt(managerQuestions).then((managerAnswers) => {
-    console.log('managerAnswers', managerAnswers
-    )
+    console.log('managerAnswers', managerAnswers)
     inquirer.prompt(addEngineerOrIntern).then((choiceAnswers) => {
         console.log('choiceAnswers', choiceAnswers
         )
         if (choiceAnswers.type === 'engineer') {
             inquirer.prompt(engineerQuestions).then((engineerAnswers) => {
                 inquirer.prompt([{ type: 'list', name: 'choice', message: "Add another team member or would you like to exit?", choices: ['Yes', 'No'] }]).then((finalAnswers) => {
-                    console.log(finalAnswers)
+                    if (finalAnswers.choice === 'Yes') {
+                        inquirer.prompt([{ type: 'list', name: 'choice', message: "Do you want to add another intern, engineer or manager or would you like to exit?", choices: ['intern', 'engineer', 'manager', 'exit'] }]).then((choiceAnswers) => {
+                            if (choiceAnswers.choice === 'intern') {
+                                inquirer.prompt(internQuestions).then((internAnswers) => {
+                                    //To Do - Run Generate page function 
+                                })
+                            }
+                        })
+                    }
                 })
             })
         } else {
@@ -102,13 +106,14 @@ inquirer.prompt(managerQuestions).then((managerAnswers) => {
                 inquirer.prompt(internQuestions).then((internAnswers) => {
                     inquirer.prompt([{ type: 'list', name: 'choice', message: "Do you want to add another intern, engineer or manager or would you like to exit?", choices: ['intern', 'engineer', 'manager', 'exit'] }]).then((finalAnswers) => {
                         if (finalAnswers.choice === 'intern') {
-                            console.log(finalAnswers)
-                        } else if{
+                            inquirer.prompt(internQuestions)
+                        } else if
+                            (finalAnswers.choice === 'engineer') {
 
-                        } else if {
+                        } else if (finalAnswers.choice === 'manager') {
 
                         } else {
-                            
+                            return
                         }
                     })
                 })
@@ -117,6 +122,50 @@ inquirer.prompt(managerQuestions).then((managerAnswers) => {
     })
 
 });
+// Function writeToFile
+function writeToFile(fileName, content) {
+    const name = fileName.toLowerCase().split(' ').join('_')
+    fs.writeFile(`./${name}.html`, content, () => {
+    })
+};
 
+// Function generateHTML content
+function generateHTML(answers) {
+    let results = ''
+    for (let i = 0; i < answers.length; i++) {
+        const result = generateEmployeeHTML(answers[i])
+        results += result
+    }
+    return results 
+}
+function generateEmployeeHTML(data) {
+    return `
+    <div>
+    <div class="item">
+        <div class="item-header">
+            <div>
+                ${data.name}
+            </div>
+            <div>
+                ${data.role}
+            </div>
+        </div>
 
-
+        <div class="item-content">
+            <div>
+                <div>ID:</div>
+                <div>${data.id}</div>
+            </div>
+            <div>
+                <div>Email:</div>
+                <div>${data.email}</div>
+            </div>
+            <div>
+                <div>Special ID:</div>
+                <div>${data.specialId}</div>
+            </div>
+        </div>
+    </div>
+</div>
+    `
+}
